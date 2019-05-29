8d3b85572a9c656402e0076523b596be8b956029
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2019-05-29 18:51:51 +0200

    Inital commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Meme-Calculator
+
+Purpose: Load to the pendrive memes which are the most valueable
+
+
+
+### How to run
+
+pip3 install -r requirements.txt
+
+python3 main.py
---
+++ main.py
@@ -0,0 +1,7 @@
+def calculate(usb_size, memes):
+    """
+    :param usb_size: size of usb stick
+    :param memes: list of tuples with memes (name, size, price)
+    :return:tuple with the first element being the total value of all memes on  the USB stick,
+    and the second being the set of names of the memes that should be copied onto the USB stick to maximize its value
+    """
